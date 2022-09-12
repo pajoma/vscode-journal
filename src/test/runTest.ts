cb27d85c6d5b324469cfc34e5159830223ac25c4
pajoma <pajoma@gmail.com>
2022-09-12 20:02:21 +0200

    Updated tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,11 +6,11 @@ async function main() {
 	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+		const extensionDevelopmentPath = path.join(__dirname, '../../');
 
 		// The path to test runner
 		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+		const extensionTestsPath = path.join(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
